aa2f40f8e4f494fb12780dc4004a0118cd66373f
myzal <myzalik@gmail.com>
2014-12-17 22:18:24 +0100

    dodanie2

--- patronat_frontend_zad1/src/app_one.js
+++ patronat_frontend_zad1/src/app_one.js
@@ -1,4 +1,4 @@
 AppOne = angular.module('AppOne', []); // rejestrujemy aplikacje pod `AppOne``
 AppOne.controller('NameController', ['$scope', function($scope){
-$scope.name = 'Wiktor'; //haszmapa odwołanie od razu do adresu 
+$scope.name = 'Mateusz'; //haszmapa odwołanie od razu do adresu 
 }]);
