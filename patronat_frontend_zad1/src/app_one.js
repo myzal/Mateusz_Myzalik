81853c1e1fc46889af8fb2f951a053aeb546eb19
myzal <myzalik@gmail.com>
2014-12-18 13:10:32 +0100

    dodanie zadanie

--- patronat_frontend_zad1/src/app_one.js
+++ patronat_frontend_zad1/src/app_one.js
@@ -2,3 +2,10 @@ AppOne = angular.module('AppOne', []); // rejestrujemy aplikacje pod `AppOne``
 AppOne.controller('NameController', ['$scope', function($scope){
 $scope.name = 'Mateusz'; //haszmapa odwołanie od razu do adresu 
 }]);
+AppOne.directive('ptrngName',function()
+{
+return{
+restrict: 'E',
+template: 'Your name is {{name}}'
+};
+});
